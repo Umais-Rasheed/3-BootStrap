419152441da35ae96977a4f70701c41ed8171814
Umais Rasheed <umaisrasheed63@gmail.com>
2024-02-14 16:13:59 +0500

    Delete App WebSite directory

--- App WebSite/assets/css/swiper-master/src/swiper-react.mjs
+++
@@ -1,6 +0,0 @@
-import { Swiper } from './react/swiper.mjs';
-import { SwiperSlide } from './react/swiper-slide.mjs';
-
-export { useSwiperSlide, useSwiper } from './react/context.mjs';
-
-export { Swiper, SwiperSlide };
--- App WebSite/assets/css/swiper-master/src/swiper-vue.mjs
+++
@@ -1,6 +0,0 @@
-import { Swiper } from './vue/swiper.mjs';
-import { SwiperSlide } from './vue/swiper-slide.mjs';
-
-export { useSwiperSlide, useSwiper } from './vue/context.mjs';
-
-export { Swiper, SwiperSlide };
